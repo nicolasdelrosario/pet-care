03574ab8f790fe3ea353f41e26764de9e8a8cea4
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-06-04 17:16:07 -0500

    feat: :sparkles: add config and env

---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+export const env = {
+  DB_CONNECTION: process.env.DB_CONNECTION,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+}
